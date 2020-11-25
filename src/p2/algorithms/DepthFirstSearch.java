b891313f33f1716117e6b8b60a61833976de0710
Knolle-03 <willem-lennart.draeger@haw-hamburg.de>
2020-11-25 19:26:40 +0100

    started working on DepthFirstSearch

---
+++ src/p2/algorithms/DepthFirstSearch.java
@@ -0,0 +1,22 @@
+package p2.algorithms;
+
+import p2.Cell;
+
+import java.util.List;
+
+public class DepthFirstSearch implements Algorithm {
+
+
+
+
+
+    @Override
+    public void calcNextStep() {
+
+    }
+
+    @Override
+    public List<Cell> getCurrentCells() {
+        return null;
+    }
+}
