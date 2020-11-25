d5ecfdf03b9cc74df910c4a9d9a2196902f4a4f1
Knolle-03 <willem-lennart.draeger@haw-hamburg.de>
2020-11-25 15:15:13 +0100

    added various booleans for drawing purposes

--- src/p2/Cell.java
+++ src/p2/Cell.java
@@ -8,11 +8,18 @@ public class Cell {
 
   PApplet sketch;
   boolean[] walls = {true, true, true, true};
+  //for generation
   boolean visited = false;
+  // for drawing purposes
   boolean isExplored = false;
+  boolean inClosedList = false;
+  boolean inOpenList = false;
+  boolean isCurrent = false;
+  boolean isPartOfPath = false;
 
 
   int manhattanDistance = -1;
+  int costOfReach = -1;
 
 
   int col;
@@ -51,25 +58,50 @@ public class Cell {
     if (manhattanDistance != -1) {
       sketch.stroke(255, 0, 0, 255);
       sketch.textAlign(PConstants.CENTER, PConstants.CENTER);
+      //sketch.textSize();
       sketch.text(manhattanDistance, x + size / 2f, y + size / 2f);
     }
 
-    if (isExplored) {
+    if (costOfReach != -1) {
+      sketch.stroke(0,0 , 255, 255);
+      sketch.textAlign(PConstants.CENTER, PConstants.CENTER);
+      //sketch.textSize();
+      sketch.text(costOfReach, x + size / 1.3f, y + size / 1.3f);
+    }
+
+    if (isCurrent) {
+      sketch.stroke(0);
+      sketch.fill(0);
+      sketch.circle(x + size / 2f,y + size / 2f,size / 2f);
+      System.out.println("Size in isCurrent draw(): " + size + "\nCol: " + x + " | Row: " + y);
+      sketch.noFill();
+    }
+
+    if (inClosedList) {
+      sketch.stroke(0,0,0,100);
+      sketch.line(x, y, x + size, y + size);
+      sketch.line(x + size, y, x, y + size);
+
+    }
+//
+    if (inOpenList) {
       sketch.noStroke();
-      sketch.fill(0, 100, 0, 100);
+      sketch.fill(0, 0, 255, 50);
       sketch.rect(x, y, size, size);
+    }
+
+    if (isPartOfPath) {
+      sketch.noStroke();
+      sketch.fill(0, 255, 0, 255);
+      sketch.rect(x + size / 10f, y + size / 10f, size - 2 * (size / 10f), size - 2 * (size / 10f));
     }
 
 
 
+
+
   }
 
-  public void highlightTarget() {
-    int x = col * size;
-    int y = row * size;
-    sketch.fill(0,0,0);
-    sketch.rect(x, y, size, size);
-  }
 
 
   public void highlight() {
@@ -94,10 +126,67 @@ public class Cell {
             ", visited=" + visited +
             ", isExplored=" + isExplored +
             ", manhattanDistance=" + manhattanDistance +
+            ", col=" + col +
+            ", row=" + row +
             '}';
+  }
+
+  //############# Getters and Setters ###################
+
+
+  public boolean isPartOfPath() {
+    return isPartOfPath;
+  }
+
+  public void setPartOfPath(boolean partOfPath) {
+    isPartOfPath = partOfPath;
+  }
+
+  public boolean isExplored() {
+    return isExplored;
+  }
+
+  public void setExplored(boolean explored) {
+    isExplored = explored;
+  }
+
+  public boolean isInClosedList() {
+    return inClosedList;
+  }
+
+  public void setInClosedList(boolean inClosedList) {
+    this.inClosedList = inClosedList;
+  }
+
+  public boolean isInOpenList() {
+    return inOpenList;
+  }
+
+  public void setInOpenList(boolean inOpenList) {
+    this.inOpenList = inOpenList;
+  }
+
+  public boolean isCurrent() {
+    return isCurrent;
+  }
+
+  public void setCurrent(boolean current) {
+    isCurrent = current;
+  }
+
+  public int getCostOfReach() {
+    return costOfReach;
+  }
+
+  public void setCostOfReach(int costOfReach) {
+    this.costOfReach = costOfReach;
   }
 
   public String get2dIndex() {
     return String.format("col: %s row: %s", col, row);
+  }
+
+  public int getManhattanDistance() {
+    return manhattanDistance;
   }
 }
